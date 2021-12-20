8c91213d8169b6c3ae7e44b6b3aac7ae35a25698
lesley <nwekelesley@gmail.com>
2021-12-20 22:35:45 +0100

    'iknvbxx'

--- celebrityapp/views.py
+++ celebrityapp/views.py
@@ -68,40 +68,6 @@ def home_view(request):
                }
     return render(request, 'account/base.html', context)
 
-@gzip.gzip_page
-def stream_view(request):
-    try:
-        cam = VideoCamera()
-        return StreamingHttpResponse(gen(cam), content_type='multipart/x-mixed-replace;boundary=frame')
-    except:
-        pass
-    return render(request,'stream.html')
-
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
-
-    def __del__(self):
-        self.video.release()
-        self.cv2.destroyAllWindows()
-
-    def get_frame(self):
-        image = self.frame
-        _, jpeg = cv2.imencode('.jpg',image)
-        return jpeg.tobytes()
-
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type:image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 
 class jview(View):
     def get(self, request, *args, **kwargs):
