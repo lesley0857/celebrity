6b962841103253e814f6800bec2e33a63027dba5
lesley <nwekelesley@gmail.com>
2021-12-20 22:26:03 +0100

    'iknvpx'

--- celebrityapp/views.py
+++ celebrityapp/views.py
@@ -79,7 +79,7 @@ def stream_view(request):
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        self.video = cv2.VideoCapture(0)
         (self.grabbed, self.frame) = self.video.read()
         threading.Thread(target=self.update, args=()).start()
 
